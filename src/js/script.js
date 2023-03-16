55390bb0e24c4553d0de2a49147308014ef3ee78
Olexandr <lisovyy13@gmail.com>
2023-03-16 22:52:36 +0200

    upd

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,8 @@
 const frontLinks = document.querySelectorAll('#front-link');
 const backLinks = document.querySelectorAll('#back-link');
 const catalogUpperContent = document.querySelectorAll('.catalog-upper');
+const btnUp = document.querySelector('.button-up');
+const preloader = document.querySelector('#preloader');
 
 function catalogLinks(linkArray) {
   linkArray.forEach((link, i) => {
@@ -15,3 +17,25 @@ function catalogLinks(linkArray) {
 
 catalogLinks(frontLinks);
 catalogLinks(backLinks);
+
+document.addEventListener('scroll', function () {
+  if (scrollY < document.documentElement.clientHeight) {
+    btnUp.style.opacity = '0';
+  } else {
+    btnUp.style.opacity = '1';
+  }
+});
+
+btnUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+window.onload = () => {
+  preloader.classList.add('hidden');
+  setTimeout(() => {
+    preloader.remove();
+  }, 500);
+};
